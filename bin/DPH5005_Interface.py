afd8e89a8c9d606023912607a6f71237124606f3
Mike Umana <mu304007@gmail.com>
2018-08-25 19:09:21 -0700

    The DPH5005 Interface script is working now. I am trying to add an indicator that when tell if the serial port is connected or not. Same with the device address. Also trying to find a good indicator for the  protection status. Maybe I will just use text color and background color. Also the CC/CV should be a label with the status.

--- bin/DPH5005_Interface.py
+++ bin/DPH5005_Interface.py
@@ -1,5 +1,4 @@
 import libscrc
-import binascii
 import struct
 import serial
 
@@ -8,33 +7,42 @@ class DPH5005:
     def __init__(self, port):
         self.port = None
         self.connect_port(port)
-        # port = serial.Serial('/dev/ttyUSB0', timeout=5)
-        # print(port.name)
         self.byte_packer = struct.Struct('>B')
         self.data_packer = struct.Struct('>H')
         self.crc_packer = struct.Struct('<H')
 
-        # Model # of DPH5005 is b'\x14\x55' in hex
-
-        # self.address = b'\x01'
+        # Model # of DPH5005 is b'\x14\x55' in hex or 5205 in decimal
 
         self.mode = {'read': b'\x03',
                      'single_write': b'\x06',
-                     'multi_write': b'\x10'}
+                     'multiple_write': b'\x10'}
 
-        self.register = {'V-SET': b'\x00\x00',
-                         'I-SET': b'\x00\x01',
-                         'V-OUT': b'\x00\x02',
-                         'I-OUT': b'\x00\x03',
-                         'POWER': b'\x00\x04',
-                         'V-IN': b'\x00\x05',
-                         'LOCK': b'\x00\x06',
-                         'PROTECT': b'\x00\x07',
-                         'CV/CC': b'\x00\x08',
-                         'ON/OFF': b'\x00\x09',
-                         'B-LED': b'\x00\x0A',
-                         'MODEL': b'\x00\x0B',
-                         'VERSION': b'\x00\x0C'}
+        self.registers = {'V-SET': b'\x00\x00',
+                          'I-SET': b'\x00\x01',
+                          'V-OUT': b'\x00\x02',
+                          'I-OUT': b'\x00\x03',
+                          'POWER': b'\x00\x04',
+                          'V-IN': b'\x00\x05',
+                          'LOCK': b'\x00\x06',
+                          'PROTECT': b'\x00\x07',
+                          'CV/CC': b'\x00\x08',
+                          'ON/OFF': b'\x00\x09',
+                          'B-LED': b'\x00\x0A',
+                          'MODEL': b'\x00\x0B',
+                          'VERSION': b'\x00\x0C'}
+        self.register_order = ['V-SET',
+                               'I-SET',
+                               'V-OUT',
+                               'I-OUT',
+                               'POWER',
+                               'V-IN',
+                               'LOCK',
+                               'PROTECT',
+                               'CV/CC',
+                               'ON/OFF',
+                               'B-LED',
+                               'MODEL',
+                               'VERSION']
 
         self.limits = {'ADDRESS': (1, 255),
                        'V-SET': (0, 50),
@@ -43,10 +51,12 @@ class DPH5005:
                        'ON/OFF': (0, 1),
                        'B-LED': (0, 5)}
 
+    # Handles connecting to a serial port. If it was connected to a previous port. it will disconnect from it first.
     def connect_port(self, port):
         self.disconnect_port()
-        self.port = serial.Serial(port, timeout=5)
+        self.port = serial.Serial(port, timeout=1)
 
+    # Handles closing the serial port if one was opened.
     def disconnect_port(self):
         if self.port is not None:
             self.port.close()
@@ -54,9 +64,6 @@ class DPH5005:
 
     def __get_crc(self, message):
         checksum = libscrc.modbus(message)
-        # print(hex(checksum))
-        # print(self.crc_packer.pack(checksum))
-        # print(self.crc_packer.unpack(self.crc_packer.pack(checksum)))
         return self.crc_packer.pack(checksum)
 
     def __send(self, command, byte_length):
@@ -70,7 +77,7 @@ class DPH5005:
         command = self.byte_packer.pack(address)
         command += self.mode[mode]
         expected_response = command
-        command += self.register[registers[0]]
+        command += self.registers[registers[0]]
         if mode == 'read':
             command += self.data_packer.pack(registers[1])
             expected_response += self.byte_packer.pack(registers[1] * 2)
@@ -78,14 +85,14 @@ class DPH5005:
                 expected_response += b'\x00\x00'
         elif mode == 'single_write':
             command += self.data_packer.pack(data[0])
-            expected_response += self.register[registers[0]]
+            expected_response += self.registers[registers[0]]
             expected_response += self.data_packer.pack(data[0])
-        elif mode == 'multi_write':
+        elif mode == 'multiple_write':
             command += self.data_packer.pack(registers[1])
             command += self.byte_packer.pack(2 * registers[1])
             for i in range(0, registers[1]):
                 command += self.data_packer.pack(data[i])
-            expected_response += self.register[registers[0]]
+            expected_response += self.registers[registers[0]]
             expected_response += self.data_packer.pack(registers[1])
         command += self.__get_crc(command)
         expected_response += self.__get_crc(expected_response)
@@ -93,19 +100,21 @@ class DPH5005:
         # Send command and save any response
         response = self.__send(command, len(expected_response))
 
-        if mode == 'read':
-            if len(response) == len(expected_response):
-                return True, self.__parse_response(response)
+        # Checks to see if the response is valid and parses it if it is.
+        if mode == 'read' and len(response) == len(expected_response):
+            return True, self.__parse_response(command, response)
         elif response[-2:] == self.__get_crc(response[:-2]):
-            return True, self.__parse_response(response)
-        return False, dict()
+            return True, self.__parse_response(command, response)
+        else:
+            return False, dict()
 
-    def __parse_response(self, response):
-        print(response)
+    def __parse_response(self, command, response):
+        parsed_data = dict()
+
         # Gets the address
         address = response[:1]
         address = self.byte_packer.unpack(address)[0]
-        print(address)
+        parsed_data['address'] = address
 
         # Gets the mode
         mode = response[1:2]
@@ -113,36 +122,56 @@ class DPH5005:
             if mode == m[1]:
                 mode = m[0]
                 break
-        print(mode)
+        parsed_data['mode'] = mode
 
         # crc = response[-2:]
         response = response[:-2]
         response = response[2:]
-        print(response)
 
-        # Gets the extra data
+        # Gets the extra data in the response. The data present depends on the mode.
         if mode == 'read':
-            pass
+            starting_register = command[2:4]
+            for item in self.registers.items():
+                if starting_register == item[1]:
+                    starting_register = item[0]
+                    break
+            number_of_registers = self.byte_packer.unpack(response[:1])[0] // 2
+            response = response[1:]
+            starting_index = self.register_order.index(starting_register)
+            parsed_data['registers'] = self.register_order[starting_index: starting_index + number_of_registers]
+            parsed_data['data'] = list()
+            for i in range(0, number_of_registers):
+                parsed_data['data'].append(self.data_packer.unpack(response[:2])[0])
+                response = response[2:]
         elif mode == 'single_write':
-            pass
-        elif mode == 'multi_write':
-            pass
+            register = response[:2]
+            parsed_data['data'] = self.data_packer.unpack(response[2:])[0]
+            for item in self.registers.items():
+                if register == item[1]:
+                    register = item[0]
+                    break
+            parsed_data['registers'] = register
+        elif mode == 'multiple_write':
+            starting_register = response[:2]
+            number_of_registers = self.data_packer.unpack(response[2:])[0]
+            for item in self.registers.items():
+                if starting_register == item[1]:
+                    starting_register = item[0]
+                    break
+            starting_index = self.register_order.index(starting_register)
+            parsed_data['registers'] = self.register_order[starting_index: starting_index + number_of_registers]
+            command = command[:-2]
+            command = command[7:]
+            parsed_data['data'] = list()
+            for i in range(0, number_of_registers):
+                parsed_data['data'].append(self.data_packer.unpack(command[:2])[0])
+                command = command[2:]
         else:
+            # It should never reach this point as it check for a valid packet, but it does hurt to have it.
             return dict()
+        return parsed_data
+
 
 if __name__ == "__main__":
-    pass
-    dph = DPH5005('COM2')
-    dph.send_command(1, 'multi_write', ['V-SET', 2], [500, 500, 3, 4])
-    # data = b'\x00\x00'
-    # data = 0
-    # command = b'\x01' + dph.mode['single_write'] + dph.register['LOCK'] + dph.data_packer.pack(data)
-
-    # crc = dph.__get_crc(command)
-
-    # command += crc
-    # print(binascii.hexlify(command))
-    # dph.port.write(command)
-    # response = dph.port.read(100)
-    # print(binascii.hexlify(response))
-    # print(command == response)
+    dph = DPH5005('COM5')
+    print(dph.send_command(1, 'read', ['PROTECT', 2], [0, 4, 3, 4]))
