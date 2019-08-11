6ced56d62562a5ed6c0d54ec670d0d49f0ead346
Mike Umana <mu304007@gmail.com>
2019-08-11 14:00:04 -0700

    command_handler.py: Flushed self.data_queue on each command
    and catch queue.Empty exception when flushing queues

--- bin/command_handler.py
+++ bin/command_handler.py
@@ -1,5 +1,5 @@
-# import queue
-# from bin.DPH5005_Interface import DPH5005
+import queue
+
 
 class Command_Handler:
     def __init__(self, command_queue, data_queue, device):
@@ -7,23 +7,25 @@ class Command_Handler:
         self.data_queue = data_queue
         self.device = device
 
-        # self.command_queue = queue.PriorityQueue()
-        # self.data_queue = queue.Queue(1)
-        # self.device = DPH5005()
-
         self.main_loop()
 
     def main_loop(self):
         while True:
             if self.device.is_port_alive():
                 cmd = self.command_queue.get()
+                if self.data_queue.full():
+                    try:
+                        self.data_queue.get_nowait()
+                    except queue.Empty:
+                        pass
                 if cmd[0] == 0:
                     while not self.command_queue.empty():
-                        self.command_queue.get_nowait()
+                        try:
+                            self.command_queue.get_nowait()
+                        except queue.Empty:
+                            pass
                 data = self.device.send_command(*cmd[2])
                 if data[1]['mode'] == 'read':
-                    if self.data_queue.full():
-                        self.data_queue.get_nowait()
                     self.data_queue.put(data)
             else:
                 while not self.command_queue.empty():
