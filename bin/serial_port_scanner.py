d8842c1cf923beaab4b0a0104cfee7b2eb42bd87
crash8229 <mu304007@gmail.com>
2021-06-18 16:40:08 -0700

    Added tty0tty ports to be scanned for linux OS

--- bin/serial_port_scanner.py
+++ bin/serial_port_scanner.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-import sys
 import glob
+import sys
+
 import serial
 
 
 def serial_ports():
-    """ Lists serial port names
+    """Lists serial port names
 
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
+    :raises EnvironmentError:
+        On unsupported or unknown platforms
+    :returns:
+        A list of the serial ports available on the system
     """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+    if sys.platform.startswith("win"):
+        ports = ["COM%s" % (i + 1) for i in range(256)]
+    elif sys.platform.startswith("linux") or sys.platform.startswith("cygwin"):
+        ports = glob.glob("/dev/tnt*")
         # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
+        ports.extend(glob.glob("/dev/tty[A-Za-z]*"))
+    elif sys.platform.startswith("darwin"):
+        ports = glob.glob("/dev/tty.*")
     else:
-        raise EnvironmentError('Unsupported platform')
+        raise EnvironmentError("Unsupported platform")
 
     result = []
     for port in ports:
@@ -30,8 +32,9 @@ def serial_ports():
             result.append(port)
         except (OSError, serial.SerialException, IOError):
             pass
+    result.reverse()
     return result
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(serial_ports())
