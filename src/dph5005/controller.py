522e3ad3d833a51ae9d4df1d2795ec3073472d0b
crash8229 <mu304007@gmail.com>
2021-07-05 19:28:18 -0700

    Laid out widgets for device connection

--- src/dph5005/controller.py
+++ src/dph5005/controller.py
@@ -13,18 +13,36 @@ import sys
 from typing import Optional, List
 
 import qdarkstyle
-from PySide2 import QtCore as QC
-from PySide2 import QtWidgets as QW
-from PySide2.QtGui import QFontDatabase
 from interface import DPH5005
+from qtpy import QtCore as QC
+from qtpy import QtGui as QG
+from qtpy import QtWidgets as QW
+from qtpy_led import Led
 from serial_port_scanner import serial_ports
 
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "")
 
 
-class IndicatorWithLabel(QW.QWidget):
-    def __init__(self, parent: Optional[QW.QWidget]):
+class LineEditWithLabel(QW.QWidget):
+    def __init__(self, label: str, parent: Optional[QW.QWidget] = None):
         super().__init__(parent)
+        layout = QW.QHBoxLayout(self)
+        layout.setSpacing(0)
+        self.label = QW.QLabel(label, self, alignment=QC.Qt.AlignRight)
+        layout.addWidget(self.label)
+        self.line_edit = QW.QLineEdit("", self, alignment=QC.Qt.AlignLeft)
+        layout.addWidget(self.line_edit)
+
+
+class IndicatorWithLabel(QW.QWidget):
+    def __init__(self, label: str, parent: Optional[QW.QWidget]):
+        super().__init__(parent)
+        layout = QW.QHBoxLayout(self)
+        layout.setSpacing(0)
+        self.label = QW.QLabel(label, self, alignment=QC.Qt.AlignRight)
+        layout.addWidget(self.label)
+        self.indicator = QW.QLabel("", self, alignment=QC.Qt.AlignLeft)
+        layout.addWidget(self.indicator)
 
 
 class DPH5005Controller(QW.QMainWindow):
@@ -40,30 +58,78 @@ class DPH5005Controller(QW.QMainWindow):
     def __gui_setup(self) -> None:
         main_tabs = QW.QTabWidget(self)
         self.setCentralWidget(main_tabs)
+        led_size = (25, 25)
 
         #### Control Tab ####
         control_window = QW.QWidget(main_tabs)
-        control_layout = QW.QGridLayout(control_window)
+        control_layout = QW.QVBoxLayout(control_window)
+        # control_layout.setContentsMargins(0, 0, 0, 0)
+        control_layout.setSpacing(0)
         main_tabs.addTab(control_window, "Control")
 
-        # Title
+        ## Title ##
         title = QW.QLabel(" DPH5005 Controller", control_window)
-        # title.setAlignment(QC.Qt.AlignCenter)
-        control_layout.addWidget(
-            title, 0, 0, columnSpan=3, alignment=QC.Qt.AlignHCenter
+        title.setMaximumHeight(18)
+        control_layout.addWidget(title, alignment=QC.Qt.AlignHCenter)
+
+        ## Device Connection ##
+        connect_row = QW.QWidget(control_window)
+        control_layout.addWidget(connect_row, alignment=QC.Qt.AlignLeft)
+        connect_row.setMaximumHeight(55)
+        connect_layout = QW.QHBoxLayout(connect_row)
+
+        # Device Address Field
+        self.address = LineEditWithLabel("Address:", connect_row)
+        connect_layout.addWidget(self.address)
+        self.address = self.address.line_edit
+        self.address.setValidator(QG.QIntValidator(1, 255, self.address))
+        self.address.setMaximumWidth(38)
+        self.address.setText("255")
+
+        # Serial Port Menu
+        serial_widget = QW.QWidget(connect_row)
+        # connect_layout.addWidget(serial_widget, alignment=QC.Qt.AlignLeft)
+        connect_layout.addWidget(serial_widget)
+        serial_layout = QW.QHBoxLayout(serial_widget)
+        serial_layout.addWidget(
+            QW.QLabel("Port:", serial_widget), alignment=QC.Qt.AlignRight
         )
+        self.serial_port_menu = QW.QComboBox(serial_widget)
+        serial_layout.addWidget(self.serial_port_menu, alignment=QC.Qt.AlignLeft)
+        self.serial_port_menu.addItems(["/dev/ttyAMA0", "1", "2", "3"])
+        self.serial_port_menu.setMinimumWidth(110)
 
-        # Close Button
-        close_button = QW.QPushButton("X", control_window)
-        close_button.setFixedSize(15, 20)
-        close_button.clicked.connect(self.close)
-        control_layout.addWidget(close_button, 0, 2)
+        # Connection Buttons
+        self.connect_buttons = QW.QStackedWidget(connect_row)
+        connect_layout.addWidget(self.connect_buttons)
+        connect_button = QW.QPushButton("Connect", self.connect_buttons)
+        disconnect_button = QW.QPushButton("Disconnect", self.connect_buttons)
+        self.connect_buttons.addWidget(connect_button)
+        self.connect_buttons.addWidget(disconnect_button)
 
-        # Serial Port Connection
+        # Separator
+        connect_layout.addWidget(QW.QLabel("|", connect_row))
 
-        # Readings
+        # Connection Status
+        port_status = QW.QHBoxLayout()
+        connect_layout.addLayout(port_status)
+        port_status.addWidget(QW.QLabel("Port Connected?:"), alignment=QC.Qt.AlignRight)
+        self.port_connected = Led(connect_row)
+        self.port_connected.setFixedSize(*led_size)
+        port_status.addWidget(self.port_connected, alignment=QC.Qt.AlignLeft)
+
+        device_status = QW.QHBoxLayout()
+        connect_layout.addLayout(device_status)
+        device_status.addWidget(
+            QW.QLabel("Device Connected?:"), alignment=QC.Qt.AlignRight
+        )
+        self.device_connected = Led(connect_row)
+        self.device_connected.setFixedSize(*led_size)
+        device_status.addWidget(self.device_connected, alignment=QC.Qt.AlignLeft)
+
+        ## Readings ##
         reading_group = QW.QGroupBox("Readings", control_window)
-        control_layout.addWidget(reading_group, 2, 0, rowSpan=3, columnSpan=3)
+        control_layout.addWidget(reading_group)
 
     @staticmethod
     def get_ports() -> List[str]:
@@ -111,7 +177,7 @@ class DPH5005Controller(QW.QMainWindow):
 
 if __name__ == "__main__":
     app = QW.QApplication([])
-    app.setFont(QFontDatabase.systemFont(QFontDatabase.FixedFont))
+    app.setFont(QG.QFontDatabase.systemFont(QG.QFontDatabase.FixedFont))
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2())
     window = DPH5005Controller()
 
